6ac206016177e156f79b7eccd3643ddef68e0333
gaganphadke17 <gaganphadke.17@gmail.com>
2024-10-18 11:49:02 +0530

    Firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,65 @@
+// login.js
+import { signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../lib/firebaseConfig';
+import { useState } from 'react';
+import { useRouter } from 'next/router'; // Import the useRouter hook from Next.js
 import styles from '../styles/LoginSignup.module.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter(); // Initialize useRouter
+
+  // Handle Email/Password login
+  const handleEmailLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in successfully
+        console.log(userCredential.user);
+        router.push('/'); // Redirect to landing page
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  // Handle Google login
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        router.push('/'); // Redirect to landing page on successful login
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div>
       <h2 className={styles.title}>Login Form</h2>
-      <form className={styles.form}>
-        <input type="email" placeholder="Email Address" required />
-        <input type="password" placeholder="Password" required />
+      <form className={styles.form} onSubmit={handleEmailLogin}>
+        <input
+          type="email"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
         <p className={styles.forgotPassword}>Forgot password?</p>
         <button type="submit" className={styles.submitButton}><span>Login</span></button>
       </form>
+      <button onClick={handleGoogleLogin} className={styles.googleButton}>
+        Sign in with Google
+      </button>
     </div>
   );
 };
---
+++ src/pages/car-detail.js
@@ -0,0 +1,13 @@
+import CarDetail from '../components/CarDetail';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function CarDetailPage() {
+  return (
+    <>
+      <Navbar />
+      <CarDetail />
+      <Footer />
+    </>
+  );
+}
